199bccf480be4db22ae00ed6b30cb9b8c8700aee
mhoow6 <mhoow6@gmail.com>
2021-04-02 14:12:57 +0900

    인터페이스 학습

---
+++ interface/interface_1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*******************************
+ * interface
+ * 계약을 정의함
+ * 해당 계약을 구현하는 class or struct는 인터페이스에 정의된 구성원의 구현을 제공해야함
+ *******************************/
+
+
+namespace Self_Study
+{
+    interface ISampleInterface
+    {
+        void SampleMethod();
+    }
+
+    class ImplementationClass : ISampleInterface
+    {
+        public void SampleMethod()
+        {
+            Console.WriteLine("Implementation completed.");
+        }
+    }
+
+    class interface_1
+    {
+        static void Main()
+        {
+            // Declare an interface instance.
+            ISampleInterface obj = new ImplementationClass();
+
+            // Call the member.
+            obj.SampleMethod();
+        }
+    }
+}
---
+++ interface/interface_2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Self_Study
+{
+    interface IPoint
+    {
+        int X
+        {
+            get;
+            set;
+        }
+
+        int Y
+        {
+            get;
+            set;
+        }
+
+        double Distance
+        {
+            get;
+        }
+    }
+
+    public struct Circle : IPoint
+    {
+        public Circle(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public double Distance => Math.Sqrt(X * X + Y * Y);
+    }
+
+    class interface_2
+    {
+        static void Main(string[] args)
+        {
+            IPoint p = new Circle();
+
+            p.X = 2;
+            p.Y = 2;
+
+            Console.WriteLine($"Circle's Distance: {p.Distance}");
+        }
+    }
+}

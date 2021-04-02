cd82840cee6f076da506b688a55e3fe9d91bc1d7
mhoow6 <mhoow6@gmail.com>
2021-04-02 14:13:36 +0900

    static 학습

---
+++ static/static_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/***************************************
+ * static
+ * 이 키워드가 붙어 있는 클래스, 함수, 변수는 프로그램이 시작할 때
+ * stack 메모리에 등록을 함 -> 인스턴스 호출하지 않아도 사용가능
+ * 
+ * 
+ * static 클래스
+ * 정적 클래스는 입력 매개변수에 대해서만 작동하고
+ * 내부 인스턴스 필드를 가져오거나 설정할 필요가 없는
+ * 메서드 집합에 대한 편리한 컨테이너로 사용할 수 있음
+ * 특정 인스턴스에 고유한 데이터를 저장하거나 검색할 필요 없이 기능을 수행하는 용도
+ ***************************************/
+
+namespace Self_Study
+{
+    class static_1
+    {
+        static void main(string[] args)
+        {
+            double dub = -3.14;
+
+            // Math 클래스는 정적 클래스라서 
+            // 인스턴스화 한 객체가 없음에도 사용가능 
+            Console.WriteLine(Math.Abs(dub));
+            Console.WriteLine(Math.Floor(dub));
+            Console.WriteLine(Math.Round(Math.Abs(dub)));
+        }
+    }
+}
---
+++ static/static_2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**********************
+ * static 멤버
+ * 생성된 클래스 인스턴스 개수에 관계 없이 정적 멤버는 한 개만 존재
+ * 전체 클래스를 static으로 선언하는 것보다 일부 정적 멤버가 포함된
+ * 비정적 클래스를 선언하는 것이 일반적
+ * 
+ * 정적 필드의 두 가지 일반적인 사용은 인스턴스화된 개체 개수를 유지하거나
+ * 모든 인스턴스 간에 공유해야 하는 값을 저장하는 것
+ **********************/
+
+
+namespace Self_Study
+{
+    public class Automobile
+    {
+        public static int NumberOfWheels = 4;
+
+        public static int SizeOfGasTank
+        {
+            get
+            {
+                return 15;
+            }
+        }
+
+        public static void Drive() { }
+
+        public int engineVer { get; set; }
+    }
+    class static_2
+    {
+       static void Main(string[] args)
+        {
+            Automobile auto = new Automobile();
+
+            auto.engineVer = 12;
+            Console.WriteLine($"{auto.engineVer}");
+
+            Automobile.NumberOfWheels = 2; // static 필드는 인스턴스로 액세스 못함
+            Console.WriteLine($"{Automobile.NumberOfWheels}");
+        }
+
+       
+    }
+}

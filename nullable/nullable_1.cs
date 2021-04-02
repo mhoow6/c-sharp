00c46681a548f7c6fdfdab9fa7e63dcd1a97681d
mhoow6 <mhoow6@gmail.com>
2021-04-02 14:13:19 +0900

    nullable 학습

---
+++ nullable/nullable_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*********************************
+ * Nullable
+ * 값 타입 변수에 null을 대입하게 해줌
+ * T? 변수이름 = null;
+ *********************************/
+
+namespace Self_Study
+{
+    class nullable_1
+    {
+        static void Main(string[] args)
+        {
+            int a = 41;
+            object aBoxed = a;
+            int? aNullable = (int?)aBoxed; // boxing 발생
+            Console.WriteLine($"Value of aNullable: {aNullable}");
+
+            int?[] arr = new int?[10];
+
+            foreach (int? element in arr)
+            {
+                Console.WriteLine($"what is in element?: {element}");
+            }
+        }
+    }
+}

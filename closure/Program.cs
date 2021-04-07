7bb96cb1254352caa9f870f6a6cfd178361b4a84
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-04-07 13:34:12 +0900

    클로져 학습

---
+++ closure/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * 클로져
+ * 함수 안의 함수를 로컬함수라 부름
+ * 무명메서드나 람다식을 정의하고 있는 메서드의 로컬변수(int key)를 사용하고 있을 때
+ * 이러한 무명메서드나 람다식을 Closure라고 부름
+ */
+
+namespace ConsoleAppClosure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = 10;
+            
+            // delegate is Closure
+            Action<string> print = delegate (string msg) {
+                string str = key + msg;
+                Console.WriteLine(str);
+            };
+
+            print("A");
+        }
+    }
+}

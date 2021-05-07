b589e1ddc1f21cf66aa2e6b068b769b58e6daf1f
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-05-07 21:24:32 +0900

    IEnumerator 인터페이스 연습

---
+++ IEnumerator/IEnumeratorExample.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+
+/* IEnumerator (이뉴머레이터)
+ * 제네릭이 아닌 컬렉션을 단순하게 반복할 수 있도록 지원하는 인터페이스
+ */
+
+public class Person
+{
+    public Person(string fName, string lName)
+    {
+        this.firstName = fName;
+        this.lastName = lName;
+    }
+
+    public string firstName;
+    public string lastName;
+}
+
+// Person 객체의 컬렉션
+// 이 클래스는 열거가능(IEnumerable)하게 만들어져
+// ForEach 구문에 사용할 수 있도록 한다.
+public class People : IEnumerable
+{
+    private Person[] _people;
+    public People(Person[] pArray)
+    {
+        _people = new Person[pArray.Length];
+
+        for (int i = 0; i < pArray.Length; i++)
+        {
+            _people[i] = pArray[i];
+        }
+    }
+
+    // 컬렉션을 반복하는 열거자 반환
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return (IEnumerator)GetEnumerator();
+    }
+
+    public PeopleEnum GetEnumerator()
+    {
+        return new PeopleEnum(_people);
+    }
+}
+
+// IEnumerable을 구현할 경우 , IEnumerator 또한 구현해야함.
+public class PeopleEnum : IEnumerator
+{
+    public Person[] _people;
+
+    // MoveNext() 호출이 있을 때까지 열거자는 첫 번째 요소 앞에 위치한다
+    int position = -1;
+
+    public PeopleEnum(Person[] list)
+    {
+        _people = list;
+    }
+
+    // 열거자를 컬렉션의 다음 요소로 이동
+    public bool MoveNext()
+    {
+        position++;
+        return (position < _people.Length);
+    }
+
+    // 컬렉션의 첫 번째 요소 앞의 초기 위치에 열거자를 설정
+    public void Reset()
+    {
+        position = -1;
+    }
+
+    // 컬렉션에서 열거자의 현재 위치에 있는 요소를 가져옴
+    // MoveNext 또는 Reset이 호출될 때까지 동일한 개체를 반환
+    public Person Current
+    {
+        get
+        {
+            try
+            {
+                return _people[position];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+    object IEnumerator.Current
+    {
+        get
+        {
+            return Current;
+        }
+    }
+}
+
+class IEnumeratorExample
+{
+    static void Main()
+    {
+        Person[] peopleArray = new Person[3]
+        {
+            new Person("John", "Smith"),
+            new Person("Jim", "Johnson"),
+            new Person("Sue", "Rabon"),
+        };
+
+        People peopleList = new People(peopleArray);
+        foreach (Person p in peopleList)
+            Console.WriteLine(p.firstName + " " + p.lastName);
+    }
+}
+
---
+++ IEnumerator/yieldExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+
+/* yield
+ * 호출자 (Caller)에게 컬렉션 데이터를 하나씩 리턴할 때 사용한다.
+ * 
+ * Enumerable 클래스에서 GetEnumerator() 메서드를 구현할 때
+ * yield return을 사용하여 컬렉선 데이터를 순차적으로 넘겨주는 코드를 구현하여
+ * 별도의 Enumerator 클래스를 작성할 필요없이 전달할 수 있다.
+ */
+
+public class MyList
+{
+    private int[] data = { 1, 2, 3, 4, 5 };
+
+    public IEnumerator GetEnumerator()
+    {
+        int i = 0;
+        while (i < data.Length)
+        {
+            yield return data[i];
+            i++;
+        }
+    }
+}
+
+
+class yieldExample
+{
+    static void Main(string[] args)
+    {
+        var list = new MyList();
+
+        // 1. For each를 사용한 Iteration
+        foreach (var item in list)
+            Console.WriteLine(item);
+
+        // 2. 수동 Iteration
+        IEnumerator it = list.GetEnumerator();
+        it.MoveNext();
+        Console.WriteLine(it.Current);
+        it.MoveNext();
+        Console.WriteLine(it.Current);
+    }
+}
+
